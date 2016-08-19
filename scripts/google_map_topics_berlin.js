4924c33464397e1f59211d09edc9e9c669054ce2
altered_conference <dax@zerowastelabs.com>
2016-08-19 11:39:48 +0200

    Extract map code

---
+++ scripts/google_map_topics_berlin.js
@@ -0,0 +1,19 @@
+function init_map(){
+  var myOptions = {
+    zoom:14,
+    center:new google.maps.LatLng(52.48352,13.441180000000031),
+    mapTypeId: google.maps.MapTypeId.ROADMAP
+  };
+
+  map = new google.maps.Map(document.getElementById("gmap_canvas"), myOptions);
+  marker = new google.maps.Marker({map: map,position: new google.maps.LatLng(52.48352, 13.441180000000031)});
+  infowindow = new google.maps.InfoWindow({content:"<b>Topics Berlin</b><br/>Weserstr 166<br/>12045 Berlin" });
+
+  google.maps.event.addListener(marker, "click", function(){
+    infowindow.open(map,marker);
+  });
+
+  infowindow.open(map,marker);
+}
+
+google.maps.event.addDomListener(window, 'load', init_map);

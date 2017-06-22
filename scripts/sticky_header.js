9eac2d6b00aa99df4421b851e4c56dd1912afce4
dax <dax@zerowastelabs.com>
2017-06-22 17:22:39 +0200

    Work on sticky header

---
+++ scripts/sticky_header.js
@@ -0,0 +1,25 @@
+// from https://stackoverflow.com/a/16442479/2128691
+
+var header = $('#dynamic-header-wrap');
+
+$(function(){
+  header.data('size','big');
+});
+
+$(window).scroll(function(){
+  if($(document).scrollTop() > 50) {
+    if(header.data('size') == 'big')
+    {
+        header.data('size','small');
+        header.stop().animate({ height:'3em' },600);
+        header.find('.inner').fadeToggle();
+    }
+  } else {
+    if(header.data('size') == 'small')
+      {
+        header.data('size','big');
+        header.stop().animate({ height:'100%' },600);
+        header.find('.inner').fadeToggle();
+      }
+  }
+});

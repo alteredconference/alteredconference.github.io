9c643849fdc42c7ea72917a9b567d3f07e009784
altered_conference <dax@zerowastelabs.com>
2017-06-23 01:48:13 +0200

    Continue work on sticky header

--- scripts/sticky_header.js
+++ scripts/sticky_header.js
@@ -6,20 +6,22 @@ $(function(){
   header.data('size','big');
 });
 
-$(window).scroll(function(){
-  if($(document).scrollTop() > 50) {
-    if(header.data('size') == 'big')
-    {
-        header.data('size','small');
-        header.stop().animate({ height:'3em' },600);
-        header.find('.inner').fadeToggle();
-    }
-  } else {
-    if(header.data('size') == 'small')
-      {
-        header.data('size','big');
-        header.stop().animate({ height:'100%' },600);
-        header.find('.inner').fadeToggle();
-      }
-  }
-});
+//$(window).scroll(function(){
+//  if($(document).scrollTop() > 150) {
+//    if(header.data('size') == 'big')
+//    {
+//        header.data('size','small');
+//        header.stop().animate({ height:'3em' },600);
+//        header.find('.inner').fadeToggle();
+//        header.find('#main-nav').fadeToggle();
+//    }
+//  } else {
+//    if(header.data('size') == 'small')
+//      {
+//        header.data('size','big');
+//        header.stop().animate({ height:'100%' },600);
+//        header.find('.inner').fadeToggle();
+//        header.find('#main-nav').fadeToggle();
+//      }
+//  }
+//});

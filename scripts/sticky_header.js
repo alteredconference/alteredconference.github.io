d738f467b00450b9aa8b92e18a54a64ff264a8fe
altered_conference <dax@zerowastelabs.com>
2017-06-24 15:05:24 +0200

    Redesign 2016 site

--- scripts/sticky_header.js
+++ scripts/sticky_header.js
@@ -6,22 +6,37 @@ $(function(){
   header.data('size','big');
 });
 
-//$(window).scroll(function(){
-//  if($(document).scrollTop() > 150) {
-//    if(header.data('size') == 'big')
-//    {
-//        header.data('size','small');
-//        header.stop().animate({ height:'3em' },600);
-//        header.find('.inner').fadeToggle();
-//        header.find('#main-nav').fadeToggle();
-//    }
-//  } else {
-//    if(header.data('size') == 'small')
-//      {
-//        header.data('size','big');
-//        header.stop().animate({ height:'100%' },600);
-//        header.find('.inner').fadeToggle();
-//        header.find('#main-nav').fadeToggle();
-//      }
-//  }
-//});
+$(document).ready(function(){
+  $('.teaser-anchor').on('click', function(){
+    var dest = $(this).attr('href'); 
+    console.log(dest); 
+    //console.log('this', $(this).href)
+    //var topPosition = $(this).data('href').offset().top;
+
+    header.fadeToggle('slow');
+    //header.find('.inner').fadeToggle();
+    //header.stop().animate({ height:'0em' },'fast');
+    //header.find('#main-nav').fadeToggle();
+    $('body').removeClass('hide-scrollbar');
+    $('#main-content').css('margin-top', '4em');
+    $('html,body').animate({ scrollTop: $(dest).offset().top }, 'slow'); 
+  });
+});
+
+$(window).scroll(function(){
+  // When the Document Object Model is ready
+  //if($(document).scrollTop() > 150) {
+  //  if(header.data('size') == 'big')
+  //  {
+  //      header.data('size','small');
+  //  }
+  //} else {
+  //  if(header.data('size') == 'small')
+  //    {
+  //      header.data('size','big');
+  //      header.stop().animate({ height:'100%' },600);
+  //      header.find('.inner').fadeToggle();
+  //      header.find('#main-nav').fadeToggle();
+  //    }
+  //}
+});

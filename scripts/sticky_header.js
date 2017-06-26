ffd28a93632c0d228be851043a38800bb6d9203e
dax <dax@zerowastelabs.com>
2017-06-26 18:11:43 +0200

    Work on nav design and break points

--- scripts/sticky_header.js
+++ scripts/sticky_header.js
@@ -7,36 +7,13 @@ $(function(){
 });
 
 $(document).ready(function(){
-  $('.teaser-anchor').on('click', function(){
+  $('.nav-anchor').on('click', function(){
     var dest = $(this).attr('href');
     console.log(dest);
-    //console.log('this', $(this).href)
-    //var topPosition = $(this).data('href').offset().top;
 
-    header.fadeToggle('slow');
-    //header.find('.inner').fadeToggle();
-    //header.stop().animate({ height:'0em' },'fast');
-    //header.find('#main-nav').fadeToggle();
+    if(!header.hasClass('hide'))  { header.addClass('hide'); };
     $('body').removeClass('hide-scrollbar');
     $('#main-content').css('margin-top', '4em');
     $('html,body').animate({ scrollTop: $(dest).offset().top - 23 }, 'fast');
   });
-});
-
-$(window).scroll(function(){
-  // When the Document Object Model is ready
-  //if($(document).scrollTop() > 150) {
-  //  if(header.data('size') == 'big')
-  //  {
-  //      header.data('size','small');
-  //  }
-  //} else {
-  //  if(header.data('size') == 'small')
-  //    {
-  //      header.data('size','big');
-  //      header.stop().animate({ height:'100%' },600);
-  //      header.find('.inner').fadeToggle();
-  //      header.find('#main-nav').fadeToggle();
-  //    }
-  //}
 });

001f3f35140e3f71830571aa7ef56133b2129f8e
altered_conference <dax@zerowastelabs.com>
2017-06-30 01:56:11 +0200

    Make nav actions more general

--- scripts/sticky_header.js
+++ scripts/sticky_header.js
@@ -1,23 +1,5 @@
 // from https://stackoverflow.com/a/16442479/2128691
 
-var header = $('#dynamic-header-wrap');
-
-$(function(){
-  header.data('size','big');
-});
-
-$(window).resize(function(){
-  var windowWidth = $(window).width();
-
-  if(windowWidth < 840) {
-    $('#nav-button').removeClass('hide');
-    $('#nav').addClass('hide');
-  } else {
-    $('#nav-button').addClass('hide');
-    $('#nav').removeClass('hide');
-  };
-});
-
 $(document).ready(function(){
   $('#nav-button').on('click', function(){
     if($('#nav').hasClass('hide')) {
@@ -29,11 +11,8 @@ $(document).ready(function(){
 
   $('.nav-anchor').on('click', function(){
     var dest = $(this).attr('href');
-    console.log(dest);
 
-    if(!header.hasClass('hide'))  { header.addClass('hide'); };
-    $('body').removeClass('hide-scrollbar');
-    $('#main-content').css('margin-top', '4em');
+    $('#nav').addClass('hide');
     $('html,body').animate({ scrollTop: $(dest).offset().top - 23 }, 'fast');
   });
 });
